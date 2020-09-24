42f702ea9203aaa95c94f8965d63d2f8a482788e
Alexey Andreyanov <54357784+AV272@users.noreply.github.com>
2020-09-24 18:47:24 +0300

    Create Function_leap_year.py

---
+++ Tasks/Function_leap_year.py
@@ -0,0 +1,26 @@
+"""
+Задача:
+Определить является ли год весокосным.
+Год весокосный если год делится на 4. 
+    За исключением случаев если год делится на 100, тогда год не весокосный.
+        За исключением случаев когда год делится на 400. Тогда год весокосный.
+"""
+
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year%4 == 0:
+        if year%100 ==0:
+            if year%400 ==0:
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+    return leap
+
+year = int(input())
