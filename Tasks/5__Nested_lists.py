ba179adf4ab3b645d4b81f8a22068b0ed0095fc4
Alexey Andreyanov <54357784+AV272@users.noreply.github.com>
2020-09-28 15:58:16 +0300

    Create 5__Nested_lists.py

---
+++ Tasks/5__Nested_lists.py
@@ -0,0 +1,22 @@
+'''
+Задача:
+Задан вложенный список из имени и оценки ученика [['alpa',1],['beta',2],['gamma',3]]. Нужно вывести имя второго с конца ученика по успеваемоти.
+Если учеников с такой оценкой несколько, то нужно вывести их имена в алфовитном порядке в отдельной строке.
+'''
+
+
+
+if __name__ == '__main__':
+    stud = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        stud.append([score,name])
+
+        
+stud.sort()
+for i in range(len(stud)):
+    if stud[i][0]>min(stud)[0]:
+        print(stud[i][1])
+        if i==len(stud)-1 or stud[i+1][0]>stud[i][0]:
+            break
