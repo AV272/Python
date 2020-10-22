d1bcbf82164861393a78b3e5dbe56fa5bf8df49d
Alexey Andreyanov <54357784+AV272@users.noreply.github.com>
2020-10-22 13:41:38 +0300

    Create _23_Built_Ins_function.py

---
+++ Tasks/_23_Built_Ins_function.py
@@ -0,0 +1,48 @@
+# Zip function #############################
+'''
+>>> print zip([1,2,3,4,5,6],'Hacker')
+[(1, 'H'), (2, 'a'), (3, 'c'), (4, 'k'), (5, 'e'), (6, 'r')]
+>>> 
+>>> print zip([1,2,3,4,5,6],[0,9,8,7,6,5,4,3,2,1])
+[(1, 0), (2, 9), (3, 8), (4, 7), (5, 6), (6, 5)]
+>>> 
+>>> A = [1,2,3]
+>>> B = [6,5,4]
+>>> C = [7,8,9]
+>>> X = [A] + [B] + [C]
+>>> 
+>>> print zip(*X)
+[(1, 6, 7), (2, 5, 8), (3, 4, 9)]
+'''
+
+stud, subj = map(int,input().split())
+
+arr = [list(map(float, input().split())) for i in range(subj)]
+for i in zip(*arr):
+    print(sum(i)/len(i))
+
+
+##################################
+# Athlete sort
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    
+    # Сортируем матрицу arr по функции key, т.е. по столбцу x[k]
+    arr.sort(key= lambda x: x[k])
+    for i in arr:
+        print(*i, sep=' ')
