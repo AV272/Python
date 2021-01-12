d1505acdf409a7a5e411981bffbf64fbe09ab735
Alexey Andreyanov <54357784+AV272@users.noreply.github.com>
2021-01-12 15:48:29 +0300

    Update _26_itertools.py

--- Tasks/_26_itertools.py
+++ Tasks/_26_itertools.py
@@ -3,3 +3,5 @@
      (permutations('abc',2) == [('a','b'), ('a','c'), ('b','a'), ('b','c'), ('c','a'), ('c','b')])
 3) itertools.groupby(iterable, key=function) - группирует элементы последовательности по значению функции key.
      Возвращает key, group = groupby(iterable) - key -- признак группировки, group -- группа элементов (необходимо переводить, например list(group))
+4) itertools.combinations('abc', 2) == [(a,b), (a,c), (b,c)] -- return all combinations of lenth 2 without repeated elements
+5) itertools.combinations_with_replacement('abc', 2) == [(a,a), (a,b), (a,c), (b,b), (b,c), (c,c)] -- return all combinations of lenth 2 with repeated elements
